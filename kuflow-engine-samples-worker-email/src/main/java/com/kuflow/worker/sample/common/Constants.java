0c25cc99af1a72fcc3e73839450105e76af9b13b
Zebensui Méndez Corujo <zmencor@kuflow.com>
2022-01-21 09:23:46 +0000

    [KF-267] Email worker sample (#1)
    * Mail example
    
    * Fill me

---
+++ kuflow-engine-samples-worker-email/src/main/java/com/kuflow/worker/sample/common/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021-present KuFlow S.L.
+ *
+ * All rights reserved.
+ */
+
+package com.kuflow.worker.sample.common;
+
+public final class Constants {
+
+    // Profiles
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+    public static final String SPRING_PROFILE_TEST = "test";
+}
---
+++ kuflow-engine-samples-worker-email/src/main/java/com/kuflow/worker/sample/config/property/ApplicationProperties.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2021-present KuFlow S.L.
+ *
+ * All rights reserved.
+ */
+
+package com.kuflow.worker.sample.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+    private final TemporalProperties temporal = new TemporalProperties();
+
+    public TemporalProperties getTemporal() {
+        return this.temporal;
+    }
+
+    public static final class TemporalProperties {
+
+        private String namespace;
+
+        private String kuflowQueue;
+
+        private String target;
+
+        private MutualTlsProperties mutualTls = new MutualTlsProperties();
+
+        public MutualTlsProperties getMutualTls() {
+            return this.mutualTls;
+        }
+
+        public String getNamespace() {
+            return this.namespace;
+        }
+
+        public void setNamespace(String namespace) {
+            this.namespace = namespace;
+        }
+
+        public String getTarget() {
+            return this.target;
+        }
+
+        public void setTarget(String target) {
+            this.target = target;
+        }
+
+        public String getKuflowQueue() {
+            return this.kuflowQueue;
+        }
+
+        public void setKuflowQueue(String kuflowQueue) {
+            this.kuflowQueue = kuflowQueue;
+        }
+
+        public static final class MutualTlsProperties {
+
+            private String ca;
+
+            private String caData;
+
+            private String cert;
+
+            private String certData;
+
+            private String key;
+
+            private String keyData;
+
+            public String getCa() {
+                return this.ca;
+            }
+
+            public void setCa(String ca) {
+                this.ca = ca;
+            }
+
+            public String getCaData() {
+                return this.caData;
+            }
+
+            public void setCaData(String caData) {
+                this.caData = caData;
+            }
+
+            public String getCert() {
+                return this.cert;
+            }
+
+            public void setCert(String cert) {
+                this.cert = cert;
+            }
+
+            public String getCertData() {
+                return this.certData;
+            }
+
+            public void setCertData(String certData) {
+                this.certData = certData;
+            }
+
+            public String getKey() {
+                return this.key;
+            }
+
+            public void setKey(String key) {
+                this.key = key;
+            }
+
+            public String getKeyData() {
+                return this.keyData;
+            }
+
+            public void setKeyData(String keyData) {
+                this.keyData = keyData;
+            }
+        }
+    }
+}
